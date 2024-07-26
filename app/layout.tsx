9f4cb7122cdc94c2be4dc80bfa86f700716f8835
djzib0 <piotrsabatura@gmail.com>
2024-07-26 19:33:53 +0200

    initializing new project, learning...

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
+// styles import
+import '@/app/ui/global.css'
+// fonts import
+import { inter } from './ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
